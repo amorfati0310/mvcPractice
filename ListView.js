6880fc4edafd4bf4d370ac0fb995fe8b2deb5f3c
chany <lyhy0310@gmail.com>
2018-06-02 20:26:20 +0900

    [fix] button 중복 이벤트 되었던 부분 fix add line-through

--- ListView.js
+++ ListView.js
@@ -3,9 +3,10 @@ class ListView {
     this.el = null;
     this.list = null;
     this.init(selector);
-  }
+  } 
   init(selector){
     this.el = document.querySelector(selector)
+    this.bindEvents()
   }
   getList(todos){
     this.list = todos;
@@ -13,12 +14,11 @@ class ListView {
   }
   updateListView(list){
     this.el.innerHTML = this.makeTemplate(list)
-    this.bindEvents()
   }
   makeTemplate(list){
     let template = ``;
     list.forEach((todo,i)=> {
-      template+= `<li><button class="complete-btn">${i}</button><p class="todo-text">${todo.text}</p><button class="delete-btn">X</button></li>`
+      template+= `<li><button class="complete-btn"></button><p class="todo-text">${todo.text}</p><button class="delete-btn">X</button></li>`
     });
     return template;
   }
@@ -27,13 +27,13 @@ class ListView {
     this.el.addEventListener('dbclick',e=>this.handleDbClick(e))
   }
   handleClicked(e){
-    debugger;
       if(e.target.className=="complete-btn") return this.handleCompleteBtnClicked(e)
       if(e.target.className=="delete-btn") return this.handleDeleteBtnClicked(e)
   }
   handleCompleteBtnClicked(e){
-    console.dir(e.target)
-    console.dir(e.target.innerText)
+    const completeBtn= e.target
+    completeBtn.innerText = "V"
+    completeBtn.nextElementSibling.classList.add('completed')
   }
   handleDeleteBtnClicked(e){
     console.dir(e.target)
